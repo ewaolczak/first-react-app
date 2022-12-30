d62464b2359c0464b93f9d2f75d3d30991c128cb
Ewa Olczak <ewa.olczak@gmail.com>
2022-12-30 17:40:33 +0100

    Add Hero, SearchForm, List and scss pack

---
+++ src/components/List/List.js
@@ -0,0 +1,19 @@
+const List = () => {
+  return (
+    <div>
+      <header>
+        <h2>
+          Things to do<span>soon</span>
+        </h2>
+      </header>
+      <p>Interesting things I want to check out</p>
+      <article>
+        <h2>Books</h2>
+        <h2>Movies</h2>
+        <h2>Games</h2>
+      </article>
+    </div>
+  );
+};
+
+export default List;
