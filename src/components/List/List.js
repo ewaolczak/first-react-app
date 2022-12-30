5d0c3d54d6f032dc88278972cbafad2bc0718b95
Ewa Olczak <ewa.olczak@gmail.com>
2022-12-30 18:10:18 +0100

    Add individual styles for Hero, SearchForm and List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,27 @@
+import styles from './List.module.scss';
+
 const List = () => {
   return (
-    <div>
-      <header>
-        <h2>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
           Things to do<span>soon</span>
         </h2>
       </header>
-      <p>Interesting things I want to check out</p>
-      <article>
-        <h2>Books</h2>
-        <h2>Movies</h2>
-        <h2>Games</h2>
-      </article>
+      <p className={styles.desctiption}>
+        Interesting things I want to check out
+      </p>
+      <section className={styles.columns}>
+        <article>
+          <h2 className={styles.title}>Books</h2>
+        </article>
+        <article>
+          <h2 className={styles.title}>Movies</h2>
+        </article>
+        <article>
+          <h2 className={styles.title}>Games</h2>
+        </article>
+      </section>
     </div>
   );
 };
