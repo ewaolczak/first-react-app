a3c2e0c336bd3d20febf000ee14f26366227788c
Ewa Olczak <ewa.olczak@gmail.com>
2022-12-31 20:36:37 +0100

    Add Card and CardForm components with styles

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,11 +1,11 @@
 import styles from './TextInput.module.scss';
 
-const TextInput = props => {
+const TextInput = (props) => {
   return (
     <input
       className={styles.input}
-      value={props.value} 
-      onChange={props.onChange} 
+      value={props.value}
+      onChange={props.onChange}
       placeholder={props.placeholder}
       type='text'
     />
