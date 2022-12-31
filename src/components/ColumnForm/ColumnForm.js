25b4798022547a11428f857d4678dfd2a9e83153
Ewa Olczak <ewa.olczak@gmail.com>
2022-12-31 18:26:26 +0100

    Add ColumnForm component with syles

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,39 @@
+import styles from './ColumnForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+import { useState } from 'react';
+
+const ColumnForm = (props) => {
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.action({ title: title, icon: icon });
+    setTitle('');
+    setIcon('');
+  };
+
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      <div>
+        <label>Title:</label>{' '}
+        <TextInput
+          type='text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Icon:</label>{' '}
+        <TextInput
+          type='text'
+          value={icon}
+          onChange={(e) => setIcon(e.target.value)}
+        />
+      </div>
+      <Button>Add column</Button>
+    </form>
+  );
+};
+
+export default ColumnForm;
