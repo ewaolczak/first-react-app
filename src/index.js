3b8c11761f30e0063370a34769a64b6237aa6980
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-08 22:25:57 +0100

    Add connection with store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import { StrictMode } from 'react';
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
