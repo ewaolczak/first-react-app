08bbcfed7a6d62dea46952909baa2e12328291a3
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-12 22:21:35 +0100

    Add development of NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
       <div className={styles.nav_wrapper}>
-        <a href='/'>
+        <Link to='/'>
           <span className='fa fa-tasks'></span>
-        </a>
+        </Link>
         <ul className={styles.nav_list}>
           <li>
-            <a href='/'>Home</a>
+            <NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to='/'>Home</NavLink>
           </li>
           <li>
-            <a href='/favorite'>Favorite</a>
+            <NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to='/favorite'>Favorite</NavLink>
           </li>
           <li>
-            <a href='/about'>About</a>
+            <NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to='/about'>About</NavLink>
           </li>
         </ul>
       </div>
