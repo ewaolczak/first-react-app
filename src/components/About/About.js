172c1c72f3fe08d20cef5448a1dba3979a5d3ebc
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-12 21:33:34 +0100

    Add PageTitle, Favorite and About components

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
+
+const About = () => {
+  return (
+    <div>
+      <PageTitle>About</PageTitle>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, vitae!
+        Hic, animi voluptates provident culpa officia ipsum veritatis soluta
+        incidunt facilis eveniet eligendi impedit debitis, sint ab rem. Optio,
+        tempora?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
+
+const Favorite = () => {
+  return (
+    <div>
+      <PageTitle>Favorite</PageTitle>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam
+        perspiciatis dolorem voluptatum ipsum repudiandae tempora impedit unde
+        explicabo. Tenetur quisquam rerum ea, nulla earum eos perspiciatis
+        quaerat quis excepturi quia!
+      </p>
+    </div>
+  );
+};
+
+export default Favorite;
